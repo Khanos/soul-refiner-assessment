8a4bc7fe554adc7ac469b7524321a61cdf4d106c
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-04-13 08:32:32 -0400

    Downgrading node version to 14, fix relative path, fix dotenv issue, project is finally running on docker

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import App from '@/app';
 import AuthRoute from '@routes/auth.route';
-import GroupsRoute from './routes/group.route';
+import GroupsRoute from '@routes/group.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
